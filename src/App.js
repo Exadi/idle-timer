325fb655aabe2dc972fa22c85e9afd90995a93e9
10799097 <hallwaywalker@gmail.com>
2019-12-30 23:27:07 -0400

    Started on navigation and a second page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React from 'react';
 //import logo from './logo.svg';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+
 import SwordFight from './Components/SwordFight'
+import Navbar from './Components/Navbar'
+import Timer from './Components/Timer/Timer'
 import './App.scss';
+import IdlingToRuleTheGods from './Components/IdlingToRuleTheGods';
+
+let selectedPage = <SwordFight/>
+let pages = [
+  {title:"Sword Fight", link: "/sword-fight", component: SwordFight},
+  {title:"ITRTG", link: "/itrtg", component: IdlingToRuleTheGods}
+]
+
+function selectPage(index){
+  selectedPage = pages[index].content
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-headers">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-      </header>
-
-      <section className="section">
-        <div className="container is-fluid">
-          <SwordFight/>
-        </div>
-      </section>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <Navbar homepage="https://exadi.github.io/idle-timer" logo="" pages={pages} />
+        <section className="section">
+          <div className="container is-fluid">
+            {pages.map((item) => (
+              <Route path={item.link} component={item.component}/>
+            ))}
+          </div>
+        </section>
+      </div>
+    </HashRouter>
+    
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import {NavLink} from 'react-router-dom'
+
+class Navbar extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+        }
+
+    }
+
+    setActiveTab(e){
+        console.log(e.target.id)
+    }
+
+    render(){
+        return (
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href={this.props.homepage}>
+                <img src={this.props.logo} width="112" height="28"></img>
+                </a>
+
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-start">
+                    {this.props.pages.map((item) => (
+                        <NavLink className="navbar-item" to={item.link}>
+                            {item.title}
+                        </NavLink>
+                    ))}
+                </div>
+
+                <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                    <a class="button is-primary">
+                        <strong>Sign up</strong>
+                    </a>
+                    <a class="button is-light">
+                        Log in
+                    </a>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </nav>
+        );
+    }
+}
+
+export default Navbar
