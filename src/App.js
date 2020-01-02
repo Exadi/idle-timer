67a55f300e8bfb91d8b30e15352a1dd75d21b187
10799097 <hallwaywalker@gmail.com>
2020-01-02 17:37:31 -0400

    added color to navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React from "react";
 //import logo from './logo.svg';
-import {
-  Route,
-  NavLink,
-  HashRouter
-} from "react-router-dom";
-import Home from './Home'
-import SwordFight from './Components/SwordFight'
-import Navbar from './Components/Navbar'
-import Timer from './Components/Timer/Timer'
-import './App.scss';
-import IdlingToRuleTheGods from './Components/IdlingToRuleTheGods';
+import { Route, NavLink, HashRouter } from "react-router-dom";
+import Home from "./Home";
+import SwordFight from "./Components/SwordFight";
+import Navbar from "./Components/Navbar";
+import Timer from "./Components/Timer/Timer";
+import "./App.scss";
+import IdlingToRuleTheGods from "./Components/IdlingToRuleTheGods";
 
 /* this array is used to create the Navbar and Routes */
 let pages = [
-  {title:"Sword Fight", link: "/sword-fight", component: SwordFight, subpages: 
-    [
-      {title:"Rival Leveling Time", link:"/sword-fight/rival-leveling", component: SwordFight}
+  {
+    title: "Sword Fight",
+    link: "/sword-fight",
+    component: SwordFight,
+    subpages: [
+      {
+        title: "Rival Leveling Time",
+        link: "/sword-fight/rival-leveling",
+        component: SwordFight
+      }
     ]
   },
-  {title:"ITRTG", link: "/itrtg", component: IdlingToRuleTheGods}
-]
+  { title: "ITRTG", link: "/itrtg", component: IdlingToRuleTheGods }
+];
 
 function App() {
   return (
@@ -28,26 +31,30 @@ function App() {
       <div className="App">
         <Navbar pages={pages} />
         <section className="section">
-          <div className="container is-fluid">
-            
-          <Route exact path="/" component={Home}/>
-            {pages.map((item) => {
+          <div className="container">
+            <Route exact path="/" component={Home} />
+            {pages.map(item => {
               return (
                 <>
-                <Route exact path={item.link} component={item.component}/>
-                {item.subpages ? item.subpages.map((subpage) => {
-                  return (
-                    <Route exact path={subpage.link} component={subpage.component}/>
-                  );
-                }): ""}
+                  <Route exact path={item.link} component={item.component} />
+                  {item.subpages
+                    ? item.subpages.map(subpage => {
+                        return (
+                          <Route
+                            exact
+                            path={subpage.link}
+                            component={subpage.component}
+                          />
+                        );
+                      })
+                    : ""}
                 </>
               );
-              })}
+            })}
           </div>
         </section>
       </div>
     </HashRouter>
-    
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,103 +1,115 @@
-import React, {Component} from 'react';
-import {NavLink} from 'react-router-dom'
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 /* navbar mobile menu toggle (unmodified example implementation from https://bulma.io/documentation/components/navbar/) */
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-  
-      // Add a click event on each of them
-      $navbarBurgers.forEach( el => {
-        el.addEventListener('click', () => {
-  
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
-  
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-  
-        });
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener("click", () => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
       });
-    }
-  
-  });
+    });
+  }
+});
 
-class Navbar extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-        }
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    }
+  setActiveTab(e) {
+    console.log(e.target.id);
+  }
 
-    setActiveTab(e){
-        console.log(e.target.id)
-    }
+  render() {
+    return (
+      <nav
+        class="navbar is-primary"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div class="container">
+          <div class="navbar-brand">
+            <NavLink className="navbar-item" to="/">
+              {this.props.logo ? (
+                <img src={this.props.logo} width="112" height="28"></img>
+              ) : (
+                "Home"
+              )}
+            </NavLink>
 
-    render(){
-        return (
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-            <div class="navbar-brand">
-                <NavLink className="navbar-item" to="/">
-                    {this.props.logo ? <img src={this.props.logo} width="112" height="28"></img> : "Home"}
-                </NavLink>
+            <a
+              role="button"
+              class="navbar-burger burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
 
-                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                </a>
+          <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+              {this.props.pages.map(item => {
+                return (
+                  /* empty root element to contain this without changing the html and misaligning the menu */
+                  <>
+                    {item.subpages ? (
+                      /*item has subpages - make dropdown*/
+                      <div class="navbar-item has-dropdown is-hoverable">
+                        <NavLink className="navbar-link" to={item.link}>
+                          {item.title}
+                        </NavLink>
+                        <div class="navbar-dropdown">
+                          {item.subpages.map(subpage => {
+                            return (
+                              <NavLink
+                                className="navbar-item"
+                                to={subpage.link}
+                              >
+                                {subpage.title}
+                              </NavLink>
+                            );
+                          })}
+                        </div>
+                      </div>
+                    ) : (
+                      /* item doesn't have subpages - just show the link */
+                      <NavLink className="navbar-item" to={item.link}>
+                        {item.title}
+                      </NavLink>
+                    )}
+                  </>
+                );
+              })}
             </div>
 
-            <div id="navbarBasicExample" class="navbar-menu">
-                <div class="navbar-start">
-                    {this.props.pages.map((item) => {
-                        return (
-                            /* empty root element to contain this without changing the html and misaligning the menu */
-                        <>
-                            
-                            {item.subpages ? 
-                            /*item has subpages - make dropdown*/
-                            (<div class="navbar-item has-dropdown is-hoverable">
-                                <NavLink className="navbar-item" to={item.link}>
-                                    {item.title}
-                                 </NavLink>
-                                 <div class="navbar-dropdown">
-                                    {item.subpages.map((subpage) => {
-                                        return (
-                                            <NavLink className="navbar-item" to={subpage.link}>
-                                                {subpage.title}
-                                            </NavLink>
-                                        );
-                                    })}
-                                </div>
-                            </div>)
-                            : 
-                            /* item doesn't have subpages - just show the link */
-                            (<NavLink className="navbar-item" to={item.link}>
-                                {item.title}
-                            </NavLink>)}
-
-                        </>
-                        );
-                        })}
-                </div>
-
-                <div class="navbar-end">
-                <div class="navbar-item">
-                    :)
-                </div>
-                </div>
+            <div class="navbar-end">
+              <div class="navbar-item">:)</div>
             </div>
-        </nav>
-        );
-    }
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
