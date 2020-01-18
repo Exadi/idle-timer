206e86da7df30ebccc8160378b002abefbadc5a5
10799097 <hallwaywalker@gmail.com>
2020-01-18 15:56:02 -0400

    hours/minutes fields focus next after 2 digits

--- src/Components/CustomTimerAdder.js
+++ src/Components/CustomTimerAdder.js
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import bell_01 from "assets/bell_01.ogg";
 import { AddTimer } from "../App";
 import { TextField, Grid, Button, Typography } from "@material-ui/core";
+
+function checkFocusNext(input, previous, nextRef) {
+  //if input is 2 digits, go to the next input, to help people enter numbers quicker but also allow them to go back and enter a 3 digit number if desired.
+
+  //don't jump to the next element if they're backspacing
+  if (input > previous) {
+    if (input >= 10 && input <= 99) {
+      nextRef.current.focus();
+    }
+  }
+}
 
 function CustomTimerAdder() {
   const timers = useSelector(state => state.timers);
@@ -11,6 +22,9 @@ function CustomTimerAdder() {
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const dispatch = useDispatch();
+  const hoursElement = useRef(null);
+  const minutesElement = useRef(null);
+  const secondsElement = useRef(null);
 
   const addButton = () => {
     let timerName = name;
@@ -37,20 +51,34 @@ function CustomTimerAdder() {
       </Grid>
       <Grid item xs={4}>
         <TextField
+          inputRef={hoursElement}
           label="Hours: "
           value={hours}
-          onChange={e => setHours(parseInt(e.target.value) || 0)}
+          onChange={e => {
+            console.log(e);
+            let input = parseInt(e.target.value);
+            let previous = hours;
+            checkFocusNext(input, previous, minutesElement);
+            setHours(input || 0);
+          }}
         ></TextField>
       </Grid>
       <Grid item xs={4}>
         <TextField
+          inputRef={minutesElement}
           label="Minutes: "
           value={minutes}
-          onChange={e => setMinutes(parseInt(e.target.value) || 0)}
+          onChange={e => {
+            let input = parseInt(e.target.value);
+            let previous = minutes;
+            checkFocusNext(input, previous, secondsElement);
+            setMinutes(input || 0);
+          }}
         ></TextField>
       </Grid>
       <Grid item xs={4}>
         <TextField
+          inputRef={secondsElement}
           label="Seconds: "
           value={seconds}
           onChange={e => setSeconds(parseInt(e.target.value) || 0)}
